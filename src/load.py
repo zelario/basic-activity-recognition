50f39e42f606830f174c26c912a9a8b490a20d66
José Amado <josegaspamado@gmail.com>
2025-09-28 15:26:16 +0100

    load

--- src/load.py
+++ src/load.py
@@ -1,5 +1,28 @@
-import csv
+
+import polars as pl
 import numpy as np
+
+def load_part_data(part_number):
+    dfs = []
+    for device in range(1, 6):
+        filename = f"data/part{part_number}/part{part_number}dev{device}.csv"
+        try:
+            df = pl.read_csv(filename, has_header=False)
+            dfs.append(df)
+        except FileNotFoundError:
+            print(f"Arquivo não encontrado: {filename}")
+        except Exception as e:
+            print(f"Erro ao ler {filename}: {e}")
+    if dfs:
+        return pl.concat(dfs).to_numpy()
+    else:
+        return np.empty((0,))
+
+def load_data():
+    full_data = [load_part_data(i) for i in range(14)]
+    return np.concatenate([d for d in full_data if d.size > 0], axis=0)
+
+'''
 
 def load_part_data(part_number):
 
@@ -22,3 +45,5 @@ def load_part_data(part_number):
 def load_data():
     full_data = [load_part_data(i) for i in range(14)]
     return np.concatenate(full_data, axis=0)
+
+'''
