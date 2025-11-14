59bf47a3d12196d802ea617ee7b97116743124dc
José Amado <josegaspamado@gmail.com>
2025-11-14 19:50:53 +0000

    s

--- src/mainB.py
+++ src/mainB.py
@@ -7,18 +7,19 @@ import numpy as np
 if __name__ == "__main__":
 
     try:
+        data = np.load("data/labels.npy")
         features_matrix = np.load("data/features_matrix.npy")
         pca_matrix = np.load("data/pca_matrix.npy")
-        labels_matrix = np.load("data/labels.npy")
-        feature_names = np.load("data/feature_names.npy", allow_pickle=True)
-        fisher_features = np.load("data/fisher_features.npy", allow_pickle=True)
-        relief_features = np.load("data/relief_features.npy", allow_pickle=True)
+        scores_matrix = np.load("data/scores_matrix.npy")
+        labels_matrix = np.load("data/labels_matrix.npy")
         print("--- Loaded data from existing .npy files ---")
+
     except FileNotFoundError:
         data = load_data_csv()
         acc_modules = variable_module(data, "Acceleration")
         mag_modules = variable_module(data, "Magnetic Field")
         gyro_modules = variable_module(data, "Angular Velocity")
+
         features_matrix, labels_matrix, feature_names = extract_features(
             data, acc_modules, mag_modules, gyro_modules,
             fs=51.5, window_duration=5.0, overlap_ratio=0.5)
@@ -31,9 +32,14 @@ if __name__ == "__main__":
         fisher_features = fisher(features_matrix, labels_matrix, feature_names)
         relief_features = relief(features_matrix, labels_matrix, feature_names=feature_names, n_neighbors=100)
 
+        scores_matrix = np.array([
+            feature_names,
+            fisher_features,
+            relief_features
+        ], dtype=object)
+
+        np.save("data/data.npy", data)
         np.save("data/features_matrix.npy", features_matrix)
         np.save("data/pca_matrix.npy", pca_matrix)
         np.save("data/labels_matrix.npy", labels_matrix)
-        np.save("data/feature_names.npy", feature_names)
-        np.save("data/fisher_features.npy", fisher_features)
-        np.save("data/relief_features.npy", relief_features)
+        np.save("data/scores_matrix.npy", scores_matrix)
